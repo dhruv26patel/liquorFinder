f2801470a8f332bcc8404b8634e03b5a81169c1f
dspatel19 <dhaval96patel@gmail.com>
2017-12-29 19:09:54 -0600

    Added location functionality and displaying names of depending on the business name.

--- LiquorFinderApp/app/src/main/java/com/app/content/liquorfinderapp/MainActivity.java
+++ LiquorFinderApp/app/src/main/java/com/app/content/liquorfinderapp/MainActivity.java
@@ -1,17 +1,30 @@
 package com.app.content.liquorfinderapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,16 +34,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputText;
-    private TextView responseView;
+    //private TextView responseView;
     private Button queryButton;
     private ProgressBar progressBar;
+    private LocationManager locationManager;
 
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    static final int REQUEST_LOCATION = 1;
     private String result;
-    private final String strUrl = "https://data.cityofchicago.org/resource/nrmj-3kcf.json?account_number=";
+    private final String strUrl = "https://data.cityofchicago.org/resource/nrmj-3kcf.json?doing_business_as_name=";
+    double latti;
+    private double longi;
 
     /**
      * Set up and bind views to their respective fields
@@ -41,13 +62,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Bind views to fields
-        inputText = (EditText)findViewById(R.id.emailText);
-        queryButton = (Button)findViewById(R.id.queryButton);
-        responseView = (TextView) findViewById(R.id.responseView);
+        inputText = (EditText) findViewById(R.id.emailText);
+        queryButton = (Button) findViewById(R.id.queryButton);
+        //responseView = (TextView) findViewById(R.id.responseView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        mainListView = (ListView) findViewById( R.id.mainListView );
 
         //Set listeners
         queryButton.setOnClickListener(submitListener);
+    }
+
+
+
+    void getLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (location != null){
+
+                latti = location.getLatitude();
+                longi = location.getLongitude();
+                Toast.makeText(MainActivity.this, "Lottitude: " + latti + " Longitute: " + longi, Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                break;
+        }
     }
 
     /**
@@ -58,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v)
         {
             new RetrieveData().execute();
+            getLocation();
         }
     };
 
@@ -68,22 +125,43 @@ public class MainActivity extends AppCompatActivity {
         try {
             //Obtain array which only has a json object in it
             JSONArray storeArray = new JSONArray(data);
-            JSONObject storeData = storeArray.getJSONObject(0);
+            ArrayList<String> planetList = new ArrayList<String>();
+//            JSONObject storeData = storeArray.getJSONObject(0);
 
+            for( int i = 0; i < storeArray.length(); i++){
 
-            //Obtains individual data as required
-            String businessName = storeData.optString("doing_business_as_name");
-            String address = storeData.optString("address");
-            String city = storeData.optString("city");
-            String state = storeData.optString("state");
-            String zip = storeData.optString("zip_code");
-            String longitude = storeData.optString("longitude");
-            String latitude = storeData.optString("latitude");
+                JSONObject storeData = storeArray.getJSONObject(i);
 
-            String fullAddress = (address + "\n" + city + ", " + state + " " + zip);
+                //Obtains individual data as required
+                String businessName = storeData.optString("doing_business_as_name");
+                String address = storeData.optString("address");
+                String city = storeData.optString("city");
+                String state = storeData.optString("state");
+                String zip = storeData.optString("zip_code");
+                String longitude = storeData.optString("longitude");
+                String latitude = storeData.optString("latitude");
 
-            responseView.setText(businessName + "\n"+ fullAddress + "\n" +
-                    "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+                Location storeLocation = new Location("storeLocation");
+                storeLocation.setLatitude(Double.parseDouble(latitude));
+                storeLocation.setLongitude(Double.parseDouble(longitude));
+
+                Location currentLocation = new Location("currentLocation");
+                currentLocation.setLatitude(latti);
+                currentLocation.setLongitude(longi);
+
+                double distance = currentLocation.distanceTo(storeLocation) / 1609.344;
+
+                String fullAddress = (address + "\n" + city + ", " + state + " " + zip);
+
+                //responseView.setText(businessName + "\n" + fullAddress + "\n" +
+                //        "Distance: " + Math.round(distance * 100) / 100 + " miles");
+
+                String str = businessName + "\n" + fullAddress + "\n" +
+                        "Distance: " + Math.round(distance * 100) / 100 + " miles";
+                planetList.add(str);
+            }
+            listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+            mainListView.setAdapter( listAdapter );
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -133,14 +211,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             //Hide keyboard
-            View view = responseView;
+            View view = mainListView;
             if (view != null) {
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
             }
             //Hide progress bar and parse data
             progressBar.setVisibility(View.GONE);
-            responseView.setText(result);
+            //responseView.setText(result);
             parseJSON(result);
         }
     }
